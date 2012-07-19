3ad6c2b7ee09fbeffb31d8790e346a4609d0f785
Sitesh Shrivastava <siteshshrivastava@gmail.com>
2012-07-19 19:34:54 +0530

    WOrking version with UI

---
+++ src/main/java/com/directi/train/tweetapp/controllers/FollowersController.java
@@ -0,0 +1,17 @@
+package com.directi.train.tweetapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sitesh
+ * Date: 19/7/12
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("/user/follower")
+public class FollowersController {
+}
---
+++ src/main/java/com/directi/train/tweetapp/controllers/FollowingController.java
@@ -0,0 +1,11 @@
+package com.directi.train.tweetapp.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sitesh
+ * Date: 19/7/12
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FollowingController {
+}

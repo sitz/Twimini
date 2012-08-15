f04ccbe5684895e2545bc7cd054a306c28e0ea51
elricL <Sriram.ga@directi.com>
2012-08-16 00:53:21 +0530

    Memcached the ShardStore

--- src/main/java/com/directi/train/tweetapp/AppConfig.java
+++ src/main/java/com/directi/train/tweetapp/AppConfig.java
@@ -1,9 +1,13 @@
 package com.directi.train.tweetapp;
 
+import net.spy.memcached.MemcachedClient;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 @Configuration
 public class AppConfig {
@@ -36,5 +40,14 @@ public class AppConfig {
         basicDataSource.setUsername("postgres");
         basicDataSource.setPassword("qwerty");
         return new SimpleJdbcTemplate(basicDataSource);
+    }
+
+    @Bean
+    public MemcachedClient memcachedClient() {
+        try {
+            return new MemcachedClient(new InetSocketAddress("localhost",11211));
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
--- src/main/java/com/directi/train/tweetapp/services/Auxillary/ShardStore.java
+++ src/main/java/com/directi/train/tweetapp/services/Auxillary/ShardStore.java
@@ -1,5 +1,6 @@
 package com.directi.train.tweetapp.services.Auxillary;
 
+import net.spy.memcached.MemcachedClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
@@ -7,8 +8,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ShardStore {
-    @Autowired private RandomStore randomStore;
-
+    @Autowired
+    private MemcachedClient memcachedClient;
     @Autowired
     @Qualifier("simpleJdbcTemplate2")
     private SimpleJdbcTemplate db2;
@@ -26,15 +27,16 @@ public class ShardStore {
     }
 
     public SimpleJdbcTemplate getShardByUserId(Long userId) {
-        return shardByIndex(shardDB.queryForInt("select shard from shards where userid = ?", userId));
+
+        return shardByIndex(cacheLayer("select shard from shards where userid = ?", userId));
     }
 
     public SimpleJdbcTemplate getShardByUserName(String userName) {
-        return shardByIndex(shardDB.queryForInt("select shard from shards where username = ?", userName));
+        return shardByIndex(cacheLayer("select shard from shards where username = ?", userName));
     }
 
     public SimpleJdbcTemplate getShardByUserEmail(String eMail) {
-        return shardByIndex(shardDB.queryForInt("select shard from shards where email = ?", eMail));
+        return shardByIndex(cacheLayer("select shard from shards where email = ?", eMail));
     }
 
     public SimpleJdbcTemplate getAuthShard() {
@@ -48,10 +50,21 @@ public class ShardStore {
         db = shardByIndex(random);
 
         shardDB.update("insert into shards (username, email, shard) values(?, ?, ?)", userName, eMail, random);
-        int userId = shardDB.queryForInt("select userid from shards where username = ?", userName);
+        int userId = cacheLayer("select userid from shards where username = ?", userName);
 
         db.update("insert into users (id, email, username, password) values(?, ?, ?, ?)", userId, eMail, userName, password);
+    }
 
+    private int cacheLayer(String query, Object arg) {
+        String argStr = arg.toString();
+        Integer result = (Integer)memcachedClient.get(query + argStr);
+        if (result!= null) {
+            System.out.println("Entering MemCached");
+            return result;
+        }
+        result = shardDB.queryForInt(query,argStr);
+        memcachedClient.set(query + argStr,3600,result);
+        return result;
     }
 
     private SimpleJdbcTemplate shardByIndex(int random) {
